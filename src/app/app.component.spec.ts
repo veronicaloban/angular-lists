32b004b1245b63bf86a60ff008589b253740684e
Roman <kaluzhenov95@gmail.com>
2020-11-03 11:32:08 +0300

    tested eslint-plugin-jasmine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,12 +13,14 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it('should have as title \'eslint\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('eslint');
   });
 
@@ -26,6 +28,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled: HTMLElement = <HTMLElement>fixture.nativeElement;
+
     expect(compiled.querySelector('.content span').textContent).toContain('eslint app is running!');
   });
 });
