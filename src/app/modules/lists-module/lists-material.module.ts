8cf22f2fa1268884ee515c8820c63f15b3bfb9b4
Veronica Loban <veronica.loban99@gmail.com>
2020-11-18 16:55:30 +0200

    styles added

---
+++ src/app/modules/lists-module/lists-material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  exports: [
+    MatCardModule,
+    MatProgressBarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+})
+export class MaterialModule {}
--- src/app/modules/lists-module/lists.module.ts
+++ src/app/modules/lists-module/lists.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ListsComponent } from './lists/lists.component';
 import { ListComponent } from './list/list.component';
 import { ListsRoutingModule } from './lists-routing.module';
+import { MaterialModule } from './lists-material.module';
 
 @NgModule({
   declarations: [ListsComponent, ListComponent],
   imports: [
     CommonModule,
     ListsRoutingModule,
+    MaterialModule,
   ],
   exports: [ListsComponent],
 })
